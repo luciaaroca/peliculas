e683aa5361e686581d21d29bc270b18ef4bd00ee
luciaaroca <lucia.aroca2510@gmail.com>
2025-10-15 14:27:46 +0200

    conseguido los filtros

--- js/peliculas.js
+++ js/peliculas.js
@@ -1,5 +1,5 @@
 const peliculas = [
-  {
+   {
     titulo: "El Padrino",
     year: 1972,
     descripcion: "La historia de la familia Corleone.",
@@ -7,19 +7,131 @@ const peliculas = [
     genero: "drama"
   },
   {
-    titulo: "Star Wars: Una nueva esperanza",
-    year: 1977,
-    descripcion: "La lucha de la Alianza Rebelde contra el Imperio.",
-    url: "https://upload.wikimedia.org/wikipedia/en/8/87/StarWarsMoviePoster1977.jpg",
+    titulo: "El Padrino II",
+    year: 1974,
+    descripcion: "La continuación de la saga de los Corleone.",
+    url: "https://upload.wikimedia.org/wikipedia/en/0/03/Godfather_part_ii.jpg",
+    genero: "drama"
+  },
+  {
+    titulo: "Harry Potter y la piedra filosofal",
+    year: 2001,
+    descripcion: "El inicio de la aventura de Harry en Hogwarts.",
+    url: "https://m.media-amazon.com/images/M/MV5BYTkyYjVkNDItODc1Ny00NzA1LTkwY2YtZDUwNTlhMDk3MTZiXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
     genero: "fantasia"
   },
   {
-    titulo: "Toy Story",
-    year: 1995,
-    descripcion: "Las aventuras de los juguetes que cobran vida.",
-    url: "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+    titulo: "Harry Potter y la cámara secreta",
+    year: 2002,
+    descripcion: "Harry regresa a Hogwarts y enfrenta nuevos peligros.",
+    url: "https://m.media-amazon.com/images/I/51UizOJ+NFL._UF1000,1000_QL80_.jpg",
+    genero: "fantasia"
+  },
+  {
+    titulo: "El Rey León",
+    year: 1994,
+    descripcion: "La historia de Simba y su camino para ser rey.",
+    url: "https://upload.wikimedia.org/wikipedia/en/3/3d/The_Lion_King_poster.jpg",
+    genero: "fantasia"
+  },
+  {
+    titulo: "El Rey León 2: El reino de Simba",
+    year: 1998,
+    descripcion: "La continuación de la historia de Simba con su hijo.",
+    url: "https://es.web.img2.acsta.net/pictures/15/06/11/11/59/479737.jpg",
+    genero: "fantasia"
+  },
+  {
+    titulo: "It",
+    year: 2017,
+    descripcion: "Un grupo de niños enfrenta al aterrador payaso Pennywise.",
+    url: "https://es.web.img3.acsta.net/medias/nmedia/18/87/80/19/19961693.jpg",
+    genero: "terror"
+  },
+  {
+    titulo: "It: Capítulo Dos",
+    year: 2019,
+    descripcion: "Los mismos personajes regresan de adultos para enfrentar a Pennywise.",
+    url: "https://es.web.img3.acsta.net/pictures/19/07/30/09/09/0763744.jpg",
+    genero: "terror"
+  },
+  {
+    titulo: "Avengers: Infinity War",
+    year: 2018,
+    descripcion: "Los héroes del universo Marvel se unen para detener a Thanos.",
+    url: "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg",
+    genero: "accion"
+  },
+  {
+    titulo: "Avengers: Endgame",
+    year: 2019,
+    descripcion: "La épica conclusión de la saga de los Vengadores.",
+    url: "https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg",
+    genero: "accion"
+  },
+  {
+    titulo: "Deadpool",
+    year: 2016,
+    descripcion: "Un antihéroe con humor negro y poderes de regeneración.",
+    url: "https://images.justwatch.com/poster/260157237/s718/deadpool.jpg",
+    genero: "accion"
+  },
+  {
+    titulo: "Mi Pobre Angelito",
+    year: 1990,
+    descripcion: "Un niño se defiende de ladrones mientras su familia está de viaje.",
+    url: "https://m.media-amazon.com/images/M/MV5BZDViOTZmYWUtNDI5ZS00N2Y3LWE0YjAtNDA2YTRjM2MwMTY4XkEyXkFqcGc@._V1_QL75_UY207_CR6,0,140,207_.jpg",
     genero: "comedia"
-  }
+  },
+   {
+    titulo: "Avatar",
+    year: 2009,
+    descripcion: "Un ex-marine se une a los Na'vi en Pandora y lucha por proteger su mundo.",
+    url: "https://es.web.img3.acsta.net/medias/nmedia/18/92/13/82/20182449.jpg",
+    genero: "fantasia"
+  },
+  {
+    titulo: "Avatar: El camino del agua",
+    year: 2022,
+    descripcion: "Jake Sully y Neytiri protegen a su familia mientras enfrentan nuevas amenazas en Pandora.",
+    url: "https://pics.filmaffinity.com/avatar_the_way_of_water-722646748-mmed.jpg",
+    genero: "fantasia"
+  },
+  {
+    titulo: "El Conjuro",
+    year: 2013,
+    descripcion: "Basado en hechos reales, un matrimonio de investigadores paranormales enfrenta una casa embrujada.",
+    url: "https://m.media-amazon.com/images/M/MV5BYWQyNTQwNTAtZDE2Yy00NjlhLWE4N2YtYjBkYTM4ZmM3ZDk0XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    genero: "terror"
+  },
+    {
+    titulo: "Jurassic Park: Parodia",
+    year: 1993,
+    descripcion: "Un divertido grupo intenta sobrevivir en un parque lleno de dinosaurios, pero con situaciones cómicas.",
+    url: "https://m.media-amazon.com/images/I/711ewSpPgUL._UF1000,1000_QL80_.jpg",
+    genero: "comedia"
+  },
+  {
+    titulo: "Shrek",
+    year: 2001,
+    descripcion: "Un ogro y sus amigos viven aventuras llenas de humor y sátira de cuentos de hadas.",
+    url: "https://images.justwatch.com/poster/327088665/s718/shrek.jpg",
+    genero: "comedia"
+  },
+  {
+  titulo: "El diario de Noa",
+  year: 2004,
+  descripcion: "La historia de un amor que perdura a lo largo de los años, enfrentando obstáculos y la memoria del tiempo.",
+  url: "https://m.media-amazon.com/images/M/MV5BZmRkMjBlOTEtZjI5OS00NTM5LWFjN2QtZjAyNTJmNTYwNzQ4XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+  genero: "drama"
+},
+ {
+    titulo: "Titanic",
+    year: 1997,
+    descripcion: "Un romance imposible surge entre Jack y Rose a bordo del fatídico Titanic.",
+    url: "https://m.media-amazon.com/images/I/71sDqDg3yRL._UF1000,1000_QL80_.jpg",
+    genero: "drama"
+  },
 ]; 
 
 export default peliculas; 
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import peliculas from "./peliculas.js"
+import peliculas from "./peliculas.js" //importamos el array de objetos peliculas
 
 const formulario = document.getElementById("formPeli"); //seleccionamos el id = "formPeli"
 const tabla = document.getElementById("tabla"); //seleccionamos el id = "tabla" (es una nueva sección)
@@ -6,55 +6,85 @@ const tabla = document.getElementById("tabla"); //seleccionamos el id = "tabla" 
 // 1 ================Regx================
 // 2 ================ARRAY================
 
-formulario.addEventListener("submit", function(event){ //escuchamos un evento (submit)
+formulario.addEventListener("submit", function(event){ //escuchamos un evento (submit) (cuando enviemos el formulario)
   event.preventDefault(); //Evita que la página envíe los datos de forma tradicional
+          
+      //cogemos los valores de cada apartado del formulario
+      const year = document.getElementById("year").value; 
+      const titulo = document.getElementById("titulo").value;
+      const descripcion= document.getElementById("descripcion").value;
+      const url = document.getElementById("url").value;
+      const genero = document.getElementById("genero").value;
       
-  //cogemos los valores de cada apartado del formulario
-  const year = document.getElementById("year").value; 
-  const titulo = document.getElementById("titulo").value;
-  const descripcion= document.getElementById("descripcion").value;
-  const url = document.getElementById("url").value;
-  const genero = document.getElementById("genero").value;
   
-  
-  //validamos que el year cumpla las dos condiciones
-  const regexAño = /^\d{4}$/; //definimos la condición
-  if(!regexAño.test(year)){//si no cumple el regex (!)
-    alert("Error: El año tiene que tener 4 cifras");
-    return; //devuelve esta alerta
-  } if (year <= 1800 && year >= 2025){ //si el num NO está entre 1800 y 2025
-    alert("Error: El año tiene que estar entre 1800 - 2025");
-    return
-  }//si ninguna de las anteriores se cumple:
-  alert('Formulario enviado correctamente.');
+      //validamos que el year cumpla las dos condiciones
+      const regexAño = /^\d{4}$/; //definimos la condición
+      if(!regexAño.test(year)){//si no cumple el regex (!)
+        alert("Error: El año tiene que tener 4 cifras");
+        return; //devuelve esta alerta
+      } if (year <= 1800 && year >= 2025){ //si el num NO está entre 1800 y 2025
+        alert("Error: El año tiene que estar entre 1800 - 2025");
+        return
+      }//si ninguna de las anteriores se cumple:
+      alert('Formulario enviado correctamente.');
 
-  //creamos el objeto pelicula
 
-  const  pelicula = {
-  titulo: titulo,
-  year: year,
-  descripcion:descripcion,
-  url: url,
-  genero: genero}
 
-  //lo añadimos al array vacío peliculas
-  peliculas.push(pelicula);
-  console.log(peliculas);
+      //creamos el objeto pelicula
+      const  pelicula = {
+      titulo: titulo,
+      year: year,
+      descripcion:descripcion,
+      url: url,
+      genero: genero}
+      //lo añadimos al array vacío peliculas
+      peliculas.push(pelicula);
+      console.log(peliculas);
 
-  //cada vez que se envie el formulario se resetea
-  formulario.reset()
- //cada vez que se envie el formulario se actualiza la tabla
-  actualizarTabla();
+      //cada vez que se envie el formulario se resetea
+      formulario.reset()
+     //cada vez que se envie el formulario se actualiza la tabla
+      actualizarTabla();
 
 }); //cerramos formulario
+
+
+
+
 
 // 3 =============TABLA================
 
 //creamos la función llamada anteriormente actualizartabla()
-function actualizarTabla (){ 
+function actualizarTabla(peliData, peliGenero){ 
   //creamos la tabla con un let ya que no va a ser una variable constante
   //primero con template strings creamos la cabecera de la tabla, lo que solo se actualiza una vez
-  let tablaPelis = `
+
+
+/////logica de hacer filtrado por peliData!!! 
+    let pelisFiltradas = [];
+
+    //para el titulo de la peli (peliData)
+    if (peliData) {
+            const tituloBuscado = peliData.trim().toLowerCase();
+            //pelisFiltradas = peliculas.filter(pelicula => pelicula.titulo.toLowerCase() === tituloBuscado);
+            pelisFiltradas = peliculas.filter(pelicula =>pelicula.titulo.toLowerCase().includes(tituloBuscado)
+);
+    } else {
+        pelisFiltradas = peliculas;
+    }
+    //para el genero de la peli (peliGenero)
+    if (peliGenero && peliGenero !== "all") {
+            const generoFiltrado = peliGenero.toLowerCase();
+            pelisFiltradas = pelisFiltradas.filter(pelicula =>pelicula.genero === generoFiltrado
+);
+     } 
+     //else {
+    //     pelisFiltradas = pelisFiltradas; //esto no hace falta
+    // }
+    
+  
+
+  let tablaPelis = ` 
 <h2> TABLA DE NUESTRAS PELIS</h2>
     <table id="tablaPelis">
       <thead>
@@ -70,7 +100,8 @@ function actualizarTabla (){
         `;
         //creamos otra función, esta vez la parte actualizable
         //para cada pelicula (objeto) le añadimos a nuestra tablaPelis anterior, el body con los valores actualizablez
-        peliculas.forEach((peli,index) => {
+        //el forEach solo acepta arrays
+        pelisFiltradas.forEach((peli,index) => { //vamos a tener peliculas filtradas
         tablaPelis += `
            <tr class="filaContenido">
              <td>${peli.titulo}</td>
@@ -98,55 +129,53 @@ function actualizarTabla (){
 
 
 
-       ////BOTON ELIMINAR////
-        const botonEditar= document.querySelector(".botonEditar");
-        botonEditar.addEventListener("click",function(){ 
-           const fila = document.querySelector(".filaContenido")
-           fila.innerHTML =`
-           <td>
-             <form class="edit-form">
-             <label>Título: <input type="text" name="titulo" value="${peli.titulo}" required></label>
-             <label>Año: <input type="number" name="year" value="${peli.year}" required></label>
-             <label>Descripción: <input type="text" name="descripcion" value="${peli.descripcion}" required></label>
-             <button type="submit">Guardar</button>
-             </form>
-           </td>
-          `;
-          const formEdit = fila.querySelector(".edit-form");
-          formEdit.addEventListener("submit", (event) =>{
-          event.preventDefault();
-          peli.titulo = formEdit.elements.titulo.value.trim();
-          peli.year = parseInt (formEdit.elements.titulo.value.trim()); //parseint - convierte un string en un num entero
-          peli.descripcion = formEdit.elements.descripcion.value.trim();
-          actualizarTabla(); // refrescamos la tabla con los cambios
-          });
+      //  ////BOTON EDITAR////
+      //   const botonEditar= document.querySelector(".botonEditar");
+      //   botonEditar.addEventListener("click",function(){ 
+      //      const fila = document.querySelector(".filaContenido")
+      //      //lo siguiente no hace falta
+      //      fila.innerHTML =`
+      //      <td>
+      //        <form class="edit-form">
+      //        <label>Título: <input type="text" name="titulo" value="${peli.titulo}" required></label>
+      //        <label>Año: <input type="number" name="year" value="${peli.year}" required></label>
+      //        <label>Descripción: <input type="text" name="descripcion" value="${peli.descripcion}" required></label>
+      //        <button type="submit">Guardar</button>
+      //        </form>
+      //      </td>
+      //     `;
+      //     const formEdit = fila.querySelector(".edit-form");
+      //     formEdit.addEventListener("submit", (event) =>{
+      //     event.preventDefault();
+      //     peli.titulo = formEdit.elements.titulo.value.trim();
+      //     peli.year = parseInt (formEdit.elements.titulo.value.trim()); //parseint - convierte un string en un num entero
+      //     peli.descripcion = formEdit.elements.descripcion.value.trim();
+      //     actualizarTabla(); // refrescamos la tabla con los cambios
+      //     });
 
 
 
-          })
-        }
-//cerramos la función actualizar tabla
+      //     })
+}//cerramos la función actualizar tabla
 actualizarTabla();
 
 
-///////////FILTRO TÍTULO///////
-function filterBytitulo(titulo) {
-    for (let i = 0; i < peliculas.length; i++) {
-        if (peliculas[i].titulo === titulo) {
-            return peliculas[i];
-        }
-    }
-    return null; // Si no se encuentra el estudiante
-}
-
-document.getElementById("titulo-filter").addEventListener("input", function() {
-    const filtro = this.value.trim();
-    actualizarTabla(filtro); // Actualiza la tabla mostrando solo las pelis que coincidan
+//---------------FILTROS COMBINADOS-----------
+//FILTRO POR TITULO
+document.getElementById("botonBuscar").addEventListener("click", function() { //del boton y click
+    const filtro = document.getElementById("buscador").value;
+    const filtroGenero = document.getElementById("genero-filter").value;
+    actualizarTabla(filtro, filtroGenero); // Actualiza la tabla mostrando solo las pelis que coincidan
+   
 });
-const peliculasFiltradas = peliculas.filter(peli => 
-    peli.titulo.toLowerCase().includes(filtro.toLowerCase())
-);
 
+
+//FILTRO POR GENERO
+document.getElementById("genero-filter").addEventListener("change", function(event) { 
+    const filtroGenero = event.target.value
+    const filtro = document.getElementById("buscador").value
+    actualizarTabla(filtro, filtroGenero); 
+});
 
 
 
